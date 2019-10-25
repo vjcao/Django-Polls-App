dd1f55b907e691e8774bc39fd16188c60e320bae
vjcao <vjcao@umich.edu>
2019-10-25 14:08:12 -0400

    changes from 10/25

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,13 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
